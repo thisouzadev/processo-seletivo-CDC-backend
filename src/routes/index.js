1514725a17a959c7bc8d7724b0866df5f7c90903
thiago <thiagodesouza.dev@gmail.com>
2022-08-04 17:31:16 -0300

    feat: change name for deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const employesController = require('../controllers/employes.controllers');
 const router = express.Router();
 
 router.post('/register', employesController.create);
-router.get('/register', employesController.getAll);
+router.get('/employers', employesController.getAll);
 router.put('/register/:id', employesController.update);
 router.delete('/register/:id', employesController.deleteEmployer);
 router.get('/register/:id', employesController.findByPrimaryKey);
